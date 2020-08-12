b6f6de9f635a1b47c81c7559bc9adec2885ec70a
akm <akm2000@gmail.com>
2020-08-12 21:58:24 +0900

    :green_heart: Refactoring before adding another case

--- calendar_test.go
+++ calendar_test.go
@@ -55,53 +55,58 @@ func TestCalendarDays(t *testing.T) {
 }
 
 func TestCalendarParse(t *testing.T) {
-	t.Run("normal", func(t *testing.T) {
-		c := NewCalendar(*NewDate(2020, 8, 1), *NewDate(2020, 8, 31), false)
-		err := c.ParseText(`
+	texts := map[string]string{
+		"full-date": `
 + 平日 : 通常営業日
 - 2020/08/10 : 山の日
 - 2020/08/17-2020/08/21 : 夏季休暇
 + 2020/08/22 : 出勤日
-`)
-		if !assert.NoError(t, err) {
-			return
-		}
+`,
+	}
 
-		if !assert.Equal(t, 4, len(c.Patterns)) {
-			return
-		}
-		if assert.IsType(t, (*Date)(nil), c.Patterns[0].DateMatcher) {
-			i := c.Patterns[0]
-			m := i.DateMatcher.(*Date)
-			assert.True(t, i.Enabled)
-			assert.Equal(t, "出勤日", i.Description)
-			assert.Equal(t, "2020-08-22", m.String())
-		}
+	for name, text := range texts {
+		t.Run(name, func(t *testing.T) {
+			c := NewCalendar(*NewDate(2020, 8, 1), *NewDate(2020, 8, 31), false)
+			err := c.ParseText(text)
+			if !assert.NoError(t, err) {
+				return
+			}
 
-		if assert.IsType(t, (*Period)(nil), c.Patterns[1].DateMatcher) {
-			i := c.Patterns[1]
-			m := i.DateMatcher.(*Period)
-			assert.False(t, i.Enabled)
-			assert.Equal(t, "夏季休暇", i.Description)
-			assert.Equal(t, "2020-08-17", m.Start.String())
-			assert.Equal(t, "2020-08-21", m.End.String())
-		}
+			if !assert.Equal(t, 4, len(c.Patterns)) {
+				return
+			}
+			if assert.IsType(t, (*Date)(nil), c.Patterns[0].DateMatcher) {
+				i := c.Patterns[0]
+				m := i.DateMatcher.(*Date)
+				assert.True(t, i.Enabled)
+				assert.Equal(t, "出勤日", i.Description)
+				assert.Equal(t, "2020-08-22", m.String())
+			}
 
-		if assert.IsType(t, (*Date)(nil), c.Patterns[2].DateMatcher) {
-			i := c.Patterns[2]
-			m := i.DateMatcher.(*Date)
-			assert.False(t, i.Enabled)
-			assert.Equal(t, "山の日", i.Description)
-			assert.Equal(t, "2020-08-10", m.String())
-		}
+			if assert.IsType(t, (*Period)(nil), c.Patterns[1].DateMatcher) {
+				i := c.Patterns[1]
+				m := i.DateMatcher.(*Period)
+				assert.False(t, i.Enabled)
+				assert.Equal(t, "夏季休暇", i.Description)
+				assert.Equal(t, "2020-08-17", m.Start.String())
+				assert.Equal(t, "2020-08-21", m.End.String())
+			}
 
-		if assert.IsType(t, (Weekdays)(nil), c.Patterns[3].DateMatcher) {
-			i := c.Patterns[3]
-			m := i.DateMatcher.(Weekdays)
-			assert.True(t, i.Enabled)
-			assert.Equal(t, "通常営業日", i.Description)
-			assert.Equal(t, Weekdays{Monday, Tuesday, Wednesday, Thursday, Friday}, m)
-		}
-	})
+			if assert.IsType(t, (*Date)(nil), c.Patterns[2].DateMatcher) {
+				i := c.Patterns[2]
+				m := i.DateMatcher.(*Date)
+				assert.False(t, i.Enabled)
+				assert.Equal(t, "山の日", i.Description)
+				assert.Equal(t, "2020-08-10", m.String())
+			}
 
+			if assert.IsType(t, (Weekdays)(nil), c.Patterns[3].DateMatcher) {
+				i := c.Patterns[3]
+				m := i.DateMatcher.(Weekdays)
+				assert.True(t, i.Enabled)
+				assert.Equal(t, "通常営業日", i.Description)
+				assert.Equal(t, Weekdays{Monday, Tuesday, Wednesday, Thursday, Friday}, m)
+			}
+		})
+	}
 }
